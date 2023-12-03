46a888687bf532ae84715471128447836dbe86e6
froehlichA <froehlich.alexander@protonmail.com>
2023-12-03 22:25:57 +0100

    fix header gen script

--- genHeader.py
+++ genHeader.py
@@ -1,13 +1,9 @@
-parts = "|".split(" ")
-
-text = parts[0]
-
+letter = "|"
+text = letter
 r = 40
 
 for i in range(r):
-  prev = parts[(1 - i) % len(parts)]
-  next = parts[(3 + i) % len(parts)]
-  text = prev + "</sup> " + text + " <sub>" + next
+  text = letter + "</sup> " + text + " <sub>" + letter
 for i in range(r):
   text = "<sup>" + text + "</sub>"
 
